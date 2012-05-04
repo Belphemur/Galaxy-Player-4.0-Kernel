319ee3f4958a98446aa215fc9a29696b50b485cc
Antoine Aflalo <antoineaf@gmail.com>
2012-05-04 17:18:36 +0200

    [SCRIPT/INITRAMFS] new way to build the kernel

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,13 +1,24 @@
 export CROSS_COMPILE=/home/balor/android/arm2009q3/bin/arm-none-eabi-
 INITRAMFS_DIR=G1initramfs
+echo "Cleaning directories"
 make ARCH=arm distclean
-make steve_defconfig
+rm -rf $INITRAMFS_DIR/*
+
+echo "Copy default initramfs"
+cp -R initramfs_root/* $INITRAMFS_DIR/
+echo "Copy configuration"
+make $1
+
 export KBUILD_BUILD_VERSION="balor_Beta_INTL"
 export LOCALVERSION="-G1XXKPN-CL562447"
+
+echo "Build kernel $KBUILD_BUILD_VERSION $LOCALVERSION with configuration $1"
 make ARCH=arm -j5
 echo "Copy modules :"
 find . -name "*.ko" ! -path "*$INITRAMFS_DIR*" -exec echo {} \;
 find . -name "*.ko" ! -path "*$INITRAMFS_DIR*" -exec cp {} $INITRAMFS_DIR/lib/modules/  \;
+
+echo "Rebuild with modules"
 make ARCH=arm -j5
 cp arch/arm/boot/zImage .
 tar cvf kernel.tar zImage
